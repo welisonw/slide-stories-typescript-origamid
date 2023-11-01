bbae6ee7432a1d4bd879fdd251a262bc393b2a42
Welison Wessling <wwelisonw@gmail.com>
2023-11-01 09:37:44 -0300

    feat(Slide): create showSlide and hideSlide methods

--- dist/modules/Slide.js
+++ dist/modules/Slide.js
@@ -3,15 +3,27 @@ export class Slide {
     elements;
     controls;
     time;
+    index;
+    slideActive;
     constructor(container, elements, controls, time = 5000) {
         this.container = container;
         this.elements = elements;
         this.controls = controls;
         this.time = time;
-        console.log(container);
-        console.log(elements);
-        console.log(controls);
-        console.log(time);
+        this.index = 0,
+            this.showSlide(this.index);
+        this.slideActive = this.elements[this.index];
+    }
+    ;
+    hideSlide(element) {
+        element.classList.remove("active");
+    }
+    ;
+    showSlide(index) {
+        this.index = index;
+        this.slideActive = this.elements[this.index];
+        this.elements.forEach(element => this.hideSlide(element));
+        this.slideActive.classList.add("active");
     }
     ;
 }
--- src/modules/Slide.ts
+++ src/modules/Slide.ts
@@ -4,22 +4,36 @@ export class Slide {
   elements;
   controls;
   time;
+  index: number;
+  slideActive: Element;
 
   constructor(
     container: Element,
     elements: Element[],
     controls: Element,
-    time: number = 5000
-  ) {
+    time: number = 5000,
+    ) {
     this.container = container;
     this.elements = elements;
     this.controls = controls;
     this.time = time;
+    this.index = 0,
+    this.showSlide(this.index);
+    this.slideActive = this.elements[this.index];
+  };
 
-    console.log(container)
-    console.log(elements)
-    console.log(controls)
-    console.log(time)
+  hideSlide(element: Element) {
+    element.classList.remove("active");
+  };
+ 
+  showSlide(index : number) {
+    this.index = index;
+    this.slideActive = this.elements[this.index]
+
+    // first removes active class from all elements, then activates it from the indicated slide
+    this.elements.forEach(element => this.hideSlide(element));
+
+    this.slideActive.classList.add("active");
   };
 
 };
